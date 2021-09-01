8b724158dce32d5096e388b3f9e6509d44f3fb6c
hyunchanpark <eye3570@gmail.com>
2021-09-01 20:54:59 +0900

    feat : rootSaga 구현

--- front/src/modules/index.ts
+++ front/src/modules/index.ts
@@ -2,12 +2,18 @@ import { combineReducers } from 'redux';
 import todosReducer from './todos';
 import filterReducer from './filter';
 import sortReducer from './sort';
+import { all, fork } from 'redux-saga/effects';
+import todoSaga from './todoSaga';
 
 const rootReducer = combineReducers({
   todos: todosReducer,
   filters: filterReducer,
   sort: sortReducer,
 });
+
+export function* rootSaga() {
+  yield all([fork(todoSaga)]);
+}
 
 export default rootReducer;
 
